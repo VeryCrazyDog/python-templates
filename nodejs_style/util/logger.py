5ae8c42822542d81298c96036b2f286edf9ea2ee
VCD <VeryCrazyDog@gmail.com>
2019-03-18 23:09:04 +0800

    Add comments

--- nodejs_style/util/logger.py
+++ nodejs_style/util/logger.py
@@ -8,7 +8,7 @@ DEFAULT_LOG_LEVEL = logging.INFO
 # Constant
 ENV_NAME_LOG_LEVEL = 'LOG_LEVEL'
 
-# Module variables
+# Internal module variable
 __log_level = {
     'DEBUG': logging.DEBUG,
     'INFO': logging.INFO,
@@ -19,8 +19,11 @@ __log_level = {
     'CRITICAL': logging.CRITICAL
 }.get(os.getenv(ENV_NAME_LOG_LEVEL, '').upper(), DEFAULT_LOG_LEVEL)
 
+# Initialize logger
 logging.basicConfig(
     level = __log_level,
     format = '%(asctime)s [%(levelname)s] %(message)s'
 )
+
+# Set logger to module variable for external import
 logger = logging.getLogger()
