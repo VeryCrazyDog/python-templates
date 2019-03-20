85e8c7d8bb2790a2528cac6b64db59198e41d8e7
VCD <VeryCrazyDog@gmail.com>
2019-03-20 23:43:24 +0800

    Change project template modularized to use simple logger

--- project/modularized/util/logger.py
+++ project/modularized/util/logger.py
@@ -1,29 +1,14 @@
 # Import build-in packages
-import os
 import logging
 
 # Module configuration
-DEFAULT_LOG_LEVEL = logging.INFO
-
-# Constant
-ENV_NAME_LOG_LEVEL = 'LOG_LEVEL'
-
-# Internal module variable
-__log_level = {
-    'DEBUG': logging.DEBUG,
-    'INFO': logging.INFO,
-    'WARN': logging.WARN,
-    'WARNING': logging.WARN,
-    'ERROR': logging.ERROR,
-    'FATAL': logging.CRITICAL,
-    'CRITICAL': logging.CRITICAL
-}.get(os.getenv(ENV_NAME_LOG_LEVEL, '').upper(), DEFAULT_LOG_LEVEL)
+LOG_LEVEL = logging.INFO
 
 # Set module variable for external import
 logger = logging.getLogger()
 
 # Initialization
 logging.basicConfig(
-    level = __log_level,
+    level = LOG_LEVEL,
     format = '%(asctime)s [%(levelname)s] %(message)s'
 )
